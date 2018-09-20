73e2d02e8f3a6e56fb2d34a83787d7e27f49a407
songdoyouho <songdoyouho@gmail.com>
2018-09-20 21:23:42 +0800

    1.0.1 hot fixfix

---
+++ secret.py
@@ -0,0 +1,3 @@
+class keys():
+    def __init__(self):
+        self.line_api_50 = 'AOVYDbdBxA4bwjWyAgjT0F1SylSjCKTMRwjf4vo/ch20BgPc5aT76qM1qM1TnZlfO/6p5CasLNMzWSPNNP+AN/r9fIteYMvlSk4bBGv0Nu/e1/AQj89SeDtcIY4sFq6uzwFwvlZYYbF/YcaCxFYRxwdB04t89/1O/w1cDnyilFU='        
